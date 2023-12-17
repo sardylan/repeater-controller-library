f140289bff276769b7339abe0dcf01eaf237ed33
Luca Cireddu <luca.cireddu@flosslab.com>
2023-12-17 18:15:55 +0100

    Adds artifact publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/sardylan/repeater-controller-library")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
